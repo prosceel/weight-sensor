b759b6c97151908c8be912768083217082729b12
Daniel Smazniy <ithesdv@gmail.com>
2023-12-20 01:05:31 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from machine import freq
+import time
+from hx711 import HX711
+
+freq(160000000)
+
+my_hx711 = HX711(pd_sck=25, dout=26)
+print("HX711 offset: %.1f" % my_hx711.offset)
+
+count = 0
+while True:
+    time.sleep(3)
+    count += 1
+    print("HX711 value %i: %.1f" % (count, my_hx711.read()))
